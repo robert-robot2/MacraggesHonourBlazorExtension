0f9ab301333b83bec8040696fe146a377ed905e7
Robert Chilewski <chilewskir@hotmail.com>
2026-08-31 15:04:40 -0400

    Add project files.

---
+++ MacraggesHonourBlazor/Program.cs
@@ -0,0 +1,15 @@
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Blazor.BrowserExtension;
+using MacraggesHonourBlazor;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.UseBrowserExtension(browserExtension =>
+{
+    builder.RootComponents.Add<App>("#app");
+    builder.RootComponents.Add<HeadOutlet>("head::after");
+});
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+await builder.Build().RunAsync();
