e60290cc7d335b88ec8981090c484c1c36c1e395
Robert Chilewski <chilewskir@hotmail.com>
2026-08-31 18:42:09 -0400

    Added Chrome API JS bridge to comunicate with background service worker

---
+++ MacraggesHonourBlazor/wwwroot/js/chromeInterop.js
@@ -0,0 +1,29 @@
+﻿window.chromeInterop = {
+    getSettings: function () {
+        return new Promise(resolve => {
+            chrome.storage.local.get(['masterEnabled', 'settings'], function (data) {
+                const s = data.settings || {};
+                resolve({
+                    masterEnabled: data.masterEnabled !== false,
+                    interceptRegular: s.interceptRegular !== false,
+                    interceptAuto: s.interceptAuto !== false,
+                    interceptEmail: s.interceptEmail !== false,
+                    showPopup: s.showPopup !== false
+                });
+            });
+        });
+    },
+    saveSettings: function (settings) {
+        return new Promise(resolve => {
+            chrome.storage.local.set({
+                masterEnabled: settings.masterEnabled,
+                settings: {
+                    interceptRegular: settings.interceptRegular,
+                    interceptAuto: settings.interceptAuto,
+                    interceptEmail: settings.interceptEmail,
+                    showPopup: settings.showPopup
+                }
+            }, resolve);
+        });
+    }
+};
